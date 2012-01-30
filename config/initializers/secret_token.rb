8eeed25cc53fec63c1665f0a1a91b4b856caad4d
brainster <brainster-one@hotmail.com>
2012-01-30 19:31:19 +0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = '53cc7d3b0fb9e03405cfd4d24c60139a450eed9951c7daa123864d8294135220a8b195010b85da36554f15b8cadaca19f25ded6241ea267c56094e27341e3e96'
