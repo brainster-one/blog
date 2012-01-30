11dbe17b6f22b69c52d50289632eab32dd5f6ce1
brainster <brainster-one@hotmail.com>
2012-01-30 20:32:43 +0700

    Routes for comments

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120130123848) do
+ActiveRecord::Schema.define(:version => 20120130131123) do
 
   create_table "articles", :force => true do |t|
     t.string   "title"
@@ -19,5 +19,15 @@ ActiveRecord::Schema.define(:version => 20120130123848) do
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
+  create_table "comments", :force => true do |t|
+    t.string   "author"
+    t.text     "content"
+    t.integer  "post_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "comments", ["post_id"], :name => "index_comments_on_post_id"
 
 end
