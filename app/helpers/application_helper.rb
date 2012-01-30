a0897ff249789fc79dc63e418ddbdb919bc445b4
brainster <brainster-one@hotmail.com>
2012-01-30 19:54:09 +0700

    Markdown, syntax highlight

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+  def markdown(text)
+    markdown = Redcarpet::Markdown.new(SyntaxHighlighter, :autolink => true, :space_after_headers => true, :fenced_code_blocks => true)
+    markdown.render(text).html_safe
+  end
+end
+
+class SyntaxHighlighter < Redcarpet::Render::HTML
+  def block_code(code, language)
+    CodeRay.scan(code, language).div
+  end
 end
